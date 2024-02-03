113f0a384be5434ab3d201411daeeee24211a050
abc <abc@gmail.com>
2024-02-03 11:59:53 -0300

    rama-inicial

---
+++ src/main/java/org/argentinaPrograma/Main.java
@@ -0,0 +1,10 @@
+package org.argentinaPrograma;
+
+import org.argentinaPrograma.Modelos.Alumno;
+
+public class Main {
+    public static void main(String[] args) {
+        Alumno alumno = new Alumno();
+        
+    }
+}
---
+++ src/main/java/org/argentinaPrograma/Modelos/Alumno.java
@@ -0,0 +1,11 @@
+package org.argentinaPrograma.Modelos;
+
+import lombok.Data;
+
+@Data
+public class Alumno {
+    private String nombre;
+    private String apellido;
+
+
+}
